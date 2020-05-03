30d6bad30e9b92eced62c4ec391ce2900a66e58e
Landon Swartz <lgsm2n@mail.missouri.edu>
2020-05-03 11:22:07 -0500

    Added some beginning of threads for reading file to main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,43 @@
 //C/C++ final project
 //Landon Swartz, Rushil Thakker, Aaron Fuller
 
+#include<iostream>
+#include<pthread.h>
+#include<vector>
+#include"fileRAII.cpp"
+
+using namespace std;
+
+#define NUM_THREADS 5 //number of threads
+
+//struct for passing to reading file. Contains file, start index, and end index
+struct ft
+{
+	char *filename;
+	int start_index;
+	int end_index;
+	//may need to add filecontents vector
+}
+
+/*Thread function that takes in arg struct with filename,
+ * start_index, and end_index to read in thread in segments
+ */
+void *readFile(void *arg)
+{`
+	int i; int c;
+	struct ft* fi = (struct ft*)arg;
+	FILE *file = fopen(fi->filename);
+	fseek(file, fi->start_index, SEEK_SET);
+	for(int i=0; i < fi->end_index - fi->start_index; i++)
+	{
+		//put into whatever to hash
+	}
+}
+
 int main()
-{	
+{
+	vector<string> fileContents;  
+
+	//reading in file
 
 }
