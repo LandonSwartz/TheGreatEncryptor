01688d2cfb7d5fa075bb74b79c901b59f6745887
Aaron Fuller <aaron.s.fuller@gmail.com>
2020-05-05 17:41:23 -0500

    init encryptionclass, not tested

---
+++ EncryptionClass.cpp
@@ -0,0 +1,198 @@
+#include<cstdlib>
+#include<pthread.h>
+#include<vector>
+#include<string>
+#include "ExceptionHandlers.h"
+#define NUM_ENCRYPTION_THREADS 10
+
+using namespace std;
+
+// The encryption class handles all encryption and decryption of the data
+class encryption_base
+{
+private:
+    // Data state has three values: 0 for data is nonexistant, 1 for encrypted data, and 2 for decrypted data
+    int data_state = 0;
+    vector<string> * data;
+    int hash_key = 0;
+
+    typedef struct algorithm_parameters {
+        // The threads will need a reference to the vector with all the data in it.
+        vector<string> * data;
+        // Tell this specific thread where to start and end.
+        int start_index;
+        int end_index;
+    } alg_prms;
+
+    // The function which does the actual encryption
+    virtual void * encryption_algorithm(void * params) {
+
+    };
+
+    bool encrypt_data() {
+        if (this->hash_key == 0) {
+            throw file_encryption_error("no hash key has been set");
+            return false;
+        }
+        if (data_state == 0) {
+            throw file_encryption_error("there is no data to encrypt");
+            return false;
+        } else if (data_state == 1) {
+            throw file_encryption_error("the data is already encrypted");
+            return false;
+        }
+
+        // Set up the structs for the threads which will encrypt the data.
+        vector<alg_prms *> encryption_alg_prms_vect;
+        int indices_per_thread;
+        int num_threads;
+        int end_additional_increment = 0;
+        // Ensure there will be at least one string per thread
+        if (data->size() >= NUM_ENCRYPTION_THREADS) {
+            indices_per_thread = (data->size()) / NUM_ENCRYPTION_THREADS;
+            end_additional_increment = (data->size()) % NUM_ENCRYPTION_THREADS;
+            num_threads = NUM_ENCRYPTION_THREADS;
+        }
+        // If there will not be one string per thread, simply use one thread
+        else {
+            num_threads = 1;
+            indices_per_thread = data->size();
+        }
+        for (int i = 0; i < num_threads; i++) {
+            alg_prms * alg_prms_strc = new alg_prms();
+            alg_prms_strc->data = this->data;
+            alg_prms_strc->start_index = (i * indices_per_thread);
+            alg_prms_strc->end_index = ((i + 1) * indices_per_thread) - 1;
+            encryption_alg_prms_vect.push_back(alg_prms_strc);
+        }
+        // Add end_additional_increment
+        encryption_alg_prms_vect.back()->end_index += end_additional_increment;
+
+        // Create the threads to encrypt the data
+        vector<pthread_t> encryption_threads;
+        for (auto prm_vct : encryption_alg_prms_vect) {
+            pthread_t new_thread;
+            pthread_create(&new_thread, NULL, this->encryption_algorithm, (void *)prm_vct);
+            encryption_threads.push_back(new_thread);
+        }
+
+        // Now that the threads are running, join them so the function will yield
+        for (auto thread : encryption_threads) {
+            pthread_join(thread, NULL);
+        }
+
+        // Delete the dynamically allocated structs once all thread operation has completed
+        for (auto prm_vct : encryption_alg_prms_vect) {
+            delete prm_vct;
+        }
+
+        // Return true to indicate the encryption was successful.
+        return true;
+    };
+    
+    virtual void * decryption_algorithm(void * params) {
+
+    };
+    
+    bool decrypt_data() {
+        if (this->hash_key == 0) {
+            throw file_decryption_error("no hash key has been set");
+            return false;
+        }
+        if (data_state == 0) {
+            throw file_decryption_error("there is no data to decrypt");
+            return false;
+        } else if (data_state == 2) {
+            throw file_decryption_error("the data has already been decrypted");
+            return false;
+        }
+        
+        // Set up the structs for the threads which will encrypt the data.
+        vector<alg_prms *> encryption_alg_prms_vect;
+        int indices_per_thread;
+        int num_threads;
+        int end_additional_increment = 0;
+        // Ensure there will be at least one string per thread
+        if (data->size() >= NUM_ENCRYPTION_THREADS) {
+            indices_per_thread = (data->size()) / NUM_ENCRYPTION_THREADS;
+            end_additional_increment = (data->size()) % NUM_ENCRYPTION_THREADS;
+            num_threads = NUM_ENCRYPTION_THREADS;
+        }
+        // If there will not be one string per thread, simply use one thread
+        else {
+            num_threads = 1;
+            indices_per_thread = data->size();
+        }
+        for (int i = 0; i < num_threads; i++) {
+            alg_prms * alg_prms_strc = new alg_prms();
+            alg_prms_strc->data = this->data;
+            alg_prms_strc->start_index = (i * indices_per_thread);
+            alg_prms_strc->end_index = ((i + 1) * indices_per_thread) - 1;
+            encryption_alg_prms_vect.push_back(alg_prms_strc);
+        }
+        // Add end_additional_increment
+        encryption_alg_prms_vect.back()->end_index += end_additional_increment;
+
+        // Create the threads to encrypt the data
+        vector<pthread_t> encryption_threads;
+        for (auto prm_vct : encryption_alg_prms_vect) {
+            pthread_t new_thread;
+            pthread_create(&new_thread, NULL, this->decryption_algorithm, (void *)prm_vct);
+            encryption_threads.push_back(new_thread);
+        }
+
+        // Now that the threads are running, join them so the function will yield
+        for (auto thread : encryption_threads) {
+            pthread_join(thread, NULL);
+        }
+
+        // Delete the dynamically allocated structs once all thread operation has completed
+        for (auto prm_vct : encryption_alg_prms_vect) {
+            delete prm_vct;
+        }
+
+        // Return true to indicate the encryption was successful.
+        return true;
+    };
+public:
+    // Give the class some data to work with, call the appropriate function so it knows if the data is encrypted
+    // or decrypted to begin with.
+    void set_encrypted_data(vector<string> * data_ref) {
+        this->data_state = 1;
+        this->data = data_ref;
+    };
+    void set_decrypted_data(vector<string> * data_ref) {
+        this->data_state = 2;
+        this->data = data_ref;
+    }
+
+    // The hash key must be set before any encryption or decryption can happen
+    void set_hash_key(int key) {
+        if (to_string(key).length() == 9)
+            this->hash_key = key;
+    };
+
+    // Note: these two getter functions must be called from within a try block
+    vector<string> * get_decrypted_data() {
+        if (data_state == 2)
+            return this->data;
+        else if (data_state == 1) {
+            this->decrypt_data();
+            return this->data;
+        } else if (data_state == 0) {
+            return nullptr;
+        }
+    };
+    vector<string> * get_encrypted_data() {
+        if (data_state == 1)
+            return this->data;
+        else if (data_state == 2) {
+            this->encrypt_data();
+            return this->data;
+        } else if (data_state == 0) {
+            return nullptr;
+        }
+    }
+
+    
+};
